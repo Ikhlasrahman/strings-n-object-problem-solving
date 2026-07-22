55ddec8e0b62658f100b36d249c8482eaaff899d
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-22 12:23:15 +0600

    first commit

---
+++ spotTheDifference.js
@@ -0,0 +1,7 @@
+
+//Task 1.1 — Spot the Difference
+// let str = "hello";
+// let arr = ["h", "e", "l", "l", "o"];
+// Log str.length and arr.length. What do you notice?
+// Try str[0] and arr[0]. Do both work the same way?
+// Try str.push("!") — what happens? Why does this fail for strings but not arrays?
