7ed59a831541546cf90b5c0c36f0a46174a375de
Ikhlas <ikhlas@Ikhlass-MacBook-Air.local>
2026-07-22 22:25:42 +0600

    profile object test

---
+++ profileObject.js
@@ -0,0 +1,13 @@
+//Task 5.1 — Build a Profile Object
+// Create an object student with these properties: name, age, grade, and isEnrolled.
+// Log the whole object.
+// Log just the name property using dot notation.
+
+let student = {
+    name: 'Ikhlas',
+    age: 18,
+    grade: 'A+',
+    isEnrolled : false
+}
+console.log(student);
+console.log(student.age);
