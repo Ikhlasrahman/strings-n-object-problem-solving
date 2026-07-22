f5f027be4bffe4dec88a09d91afc31db314bd85a
Ikhlas <ikhlas@Ikhlass-MacBook-Air.local>
2026-07-23 01:53:52 +0600

    loop

---
+++ loopThroughProperties.js
@@ -0,0 +1,21 @@
+// Task 8.1 — Loop Through Properties
+// Use a for...in loop to log each key and value like: math: 90.
+// Calculate the average of all values in the loop.
+
+let scores ={
+    math:90,
+    science:85,
+    art:95
+}
+
+let total = 0;
+
+
+for (let key in scores){
+
+    console.log(key,":",scores[key]);
+    total = scores[key]+total;
+    
+}
+
+console.log(total);
