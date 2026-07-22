b5dbe64de921450ffeb52ed8a20493083fbf3ace
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-22 17:43:47 +0600

    ConverBetweenThem

---
+++ convertBetweenThem.js
@@ -0,0 +1,12 @@
+// Task 1.2 — Convert Between Them
+
+// Convert the string "JavaScript" into an array of characters.
+const Strings = "JavaScript";
+const arr = Strings.split("");
+console.log(arr);
+
+// Convert the array ["c", "a", "t"] back into a single string "cat".
+
+const toJoin = ["c", "a", "t"];
+const singleString = toJoin.join('');
+console.log(singleString);
