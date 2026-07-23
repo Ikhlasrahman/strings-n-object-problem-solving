ff5cf798fdb7e69e66b777dc65f625aadd60a2ac
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-23 10:01:02 +0600

    Solve Loop thoug properties

--- loopThroughProperties.js
+++ loopThroughProperties.js
@@ -2,20 +2,17 @@
 // Use a for...in loop to log each key and value like: math: 90.
 // Calculate the average of all values in the loop.
 
-let scores ={
-    math:90,
-    science:85,
-    art:95
-}
+let scores = {
+  math: 90,
+  science: 85,
+  art: 95,
+};
 
 let total = 0;
 
-
-for (let key in scores){
-
-    console.log(key,":",scores[key]);
-    total = scores[key]+total;
-    
+for (let key in scores) {
+  console.log(key, ":", scores[key]);
+  total = scores[key] + total;
 }
 
-console.log(total);
+console.log(total / Object.keys(scores).length);
